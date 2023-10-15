50208a1926fc56d6ad4004cfd77ec48283a73be0
Jose L Mortola <jose.mortola@primaverasound.com>
2023-10-15 16:37:26 +0200

    feat: add basic dashboard layout (#6)

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,8 @@
-export default function Dashboard() {
-  return 'dash';
+import client from './client';
+import Content from './content';
+
+export default async function Dashboard() {
+  // Prefetch initial data serverside
+  const initialPostData = await client('/posts');
+  return <Content initialPostData={initialPostData} />;
 }
