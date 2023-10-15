b35f92f09afc970e3631c816dd7c002db74866f8
Jose L Mortola <jose.mortola@primaverasound.com>
2023-10-15 11:28:08 +0200

    feat: add upload image logic

---
+++ app/api/auth/[kindeAuth]/upload/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from 'next/server';
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+function isBase64Image(data: string) {
+  if (typeof data !== 'string') return false;
+  if (data.startsWith('data:image')) data = data.split(',')[1];
+  const base64Regex = /^[A-Za-z0-9+/]*={0,2}$/;
+  return base64Regex.test(data) && (data.length % 4 === 0); // Check if length is multiple of 4
+}
+
+const cloudinaryConfig = {
+  folder: 'store'
+}
+
+export async function POST(req: Request) {
+  if (req.method === 'POST') {
+    const { image } = await req.json();
+    // TODO: accept different file types (pdf, video, etc...)
+    const isValidImage = isBase64Image(image)
+    if (!isValidImage) return NextResponse.json({error: 'Image should be base64 encoded'}, { status: 400 })
+    const uploadResponse = await cloudinary.uploader.upload(image, cloudinaryConfig);
+    return NextResponse.json({ url: uploadResponse.secure_url }, { status: 200 });
+  } else {
+    return NextResponse.json({ error: `Method ${req.method} Not Allowed` }, { status: 405 })
+  }
+}
