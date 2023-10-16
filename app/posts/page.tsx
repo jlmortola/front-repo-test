b96607f8412e835b96f33e1715ed547db053424b
jose.mortola <jose.mortola@primaverasound.com>
2023-10-17 00:46:02 +0200

    fix: add error habdler to initial fetch

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -3,6 +3,6 @@ import Content from './content';
 
 export default async function Dashboard() {
   // Prefetch initial data serverside
-  const initialPostData = await client('/posts');
+  const initialPostData = await client('/posts').catch((err) => console.log(err));
   return <Content initialPostData={initialPostData} />;
 }
